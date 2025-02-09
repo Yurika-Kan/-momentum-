5fca80202ed57b7770d107c91ec363c6c5f80a1f
LondonJones <londonjonez00@gmail.com>
2025-02-09 04:18:03 -0500

    resources + create
    added text for resources + create

--- prisma/migrations/20250209081521_init/migration.sql
+++ prisma/migrations/20250209081521_init/migration.sql
@@ -17,11 +17,11 @@ CREATE TABLE "Tag" (
 );
 
 -- CreateTable
-CREATE TABLE "Tagged" (
+CREATE TABLE "TagsOnPost" (
     "prop_id" INTEGER NOT NULL,
     "tag" TEXT NOT NULL,
 
-    CONSTRAINT "Tagged_pkey" PRIMARY KEY ("prop_id","tag")
+    CONSTRAINT "TagsOnPost_pkey" PRIMARY KEY ("prop_id","tag")
 );
 
 -- CreateTable
@@ -43,28 +43,20 @@ CREATE TABLE "Teammate" (
 -- CreateTable
 CREATE TABLE "User" (
     "username" TEXT NOT NULL,
-    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "mentor_mode" BOOLEAN NOT NULL,
+    "contact_info" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("username")
 );
 
 -- CreateTable
-CREATE TABLE "Account" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "provider" TEXT NOT NULL,
-    "providerAccountId" TEXT NOT NULL,
-    "refresh_token" TEXT,
-    "access_token" TEXT,
-    "expires_at" INTEGER,
-    "token_type" TEXT,
-    "scope" TEXT,
-    "id_token" TEXT,
-    "session_state" TEXT,
-    "refresh_token_expires_in" INTEGER,
+CREATE TABLE "TagsOnUser" (
+    "Tag" TEXT NOT NULL,
+    "user" TEXT NOT NULL,
 
-    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "TagsOnUser_pkey" PRIMARY KEY ("Tag")
 );
 
 -- CreateTable
@@ -84,17 +76,6 @@ CREATE TABLE "VerificationToken" (
     "expires" TIMESTAMP(3) NOT NULL
 );
 
--- CreateTable
-CREATE TABLE "_ProposalToTag" (
-    "A" INTEGER NOT NULL,
-    "B" TEXT NOT NULL,
-
-    CONSTRAINT "_ProposalToTag_AB_pkey" PRIMARY KEY ("A","B")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
-
 -- CreateIndex
 CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
 
@@ -104,14 +85,14 @@ CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token"
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
 
--- CreateIndex
-CREATE INDEX "_ProposalToTag_B_index" ON "_ProposalToTag"("B");
+-- AddForeignKey
+ALTER TABLE "Proposal" ADD CONSTRAINT "Proposal_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("username") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Tagged" ADD CONSTRAINT "Tagged_prop_id_fkey" FOREIGN KEY ("prop_id") REFERENCES "Proposal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "TagsOnPost" ADD CONSTRAINT "TagsOnPost_prop_id_fkey" FOREIGN KEY ("prop_id") REFERENCES "Proposal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Tagged" ADD CONSTRAINT "Tagged_tag_fkey" FOREIGN KEY ("tag") REFERENCES "Tag"("tag") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "TagsOnPost" ADD CONSTRAINT "TagsOnPost_tag_fkey" FOREIGN KEY ("tag") REFERENCES "Tag"("tag") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Request" ADD CONSTRAINT "Request_prop_id_fkey" FOREIGN KEY ("prop_id") REFERENCES "Proposal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -126,13 +107,7 @@ ALTER TABLE "Teammate" ADD CONSTRAINT "Teammate_prop_id_fkey" FOREIGN KEY ("prop
 ALTER TABLE "Teammate" ADD CONSTRAINT "Teammate_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("username") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("username") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "TagsOnUser" ADD CONSTRAINT "TagsOnUser_user_fkey" FOREIGN KEY ("user") REFERENCES "User"("username") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("username") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_ProposalToTag" ADD CONSTRAINT "_ProposalToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Proposal"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_ProposalToTag" ADD CONSTRAINT "_ProposalToTag_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("tag") ON DELETE CASCADE ON UPDATE CASCADE;
